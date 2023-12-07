90b209f2e937a80375a097a820a93c97c1941100
MohamedAmineALLAF <amine.allafi@gmail.com>
2023-12-07 09:36:50 +0100

    order service : Service for handling customer orders, ensuring seamless communication with related services

---
+++ order-service/src/main/java/org/sid/orderservice/entities/Order.java
@@ -0,0 +1,34 @@
+package org.sid.orderservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.orderservice.enums.OrderStatus;
+import org.sid.orderservice.model.Customer;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+@Entity
+@Table(name="orders")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Order {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date createdAt;
+    private OrderStatus status;
+    private Long customerId;
+    @Transient
+    private Customer customer;
+    @OneToMany(mappedBy = "order")
+    private List<ProductItem> productItems;
+
+    public double getTotal(){
+        double somme=0;
+        for(ProductItem pi:productItems){
+            somme+=pi.getAmount();
+        }
+        return somme;
+    }
+}
